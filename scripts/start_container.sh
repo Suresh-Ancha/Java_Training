b7095235483ef4719f9a0fc90c6bd73b829b2f76
Suresh-Ancha <134360427+Suresh-Ancha@users.noreply.github.com>
2024-01-30 14:59:36 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull sureshancha/simple-java-flask-app
+docker pull sureshancha/simple-java-flask-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 sureshancha/simple-java-flask-app
+docker run -d -p 5000:5000 sureshancha/simple-java-flask-app:latest
